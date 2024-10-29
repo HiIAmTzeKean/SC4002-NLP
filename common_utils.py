c5269fd9c5c9f9b2a1aed6eec645eaa2352c103f
HiIAmTzeKean <ngtzekean@gmail.com>
2024-10-29 20:12:30 +0800

    fix: common_util

--- common_utils.py
+++ common_utils.py
@@ -76,7 +76,7 @@ class EmbeddingMatrix():
         self.embedding_matrix:np.ndarray
         self.word2idx:dict
     @classmethod
-    def load() -> "EmbeddingMatrix":
+    def load(cls) -> "EmbeddingMatrix":
         # load vectors from file
         embedding_matrix:np.ndarray = np.load(EMBEDDING_MATRIX_PATH)
         # set attributes
@@ -90,12 +90,29 @@ class EmbeddingMatrix():
         return em
     @property
     def dimension(self) -> int:
+        """Dimension of the embedding matrix
+        
+        :return: The dimension of the embedding matrix
+        :rtype: int
+        """
         return self.d
     @property
     def vocab_size(self) -> int:
+        """Vocabulary size of the embedding matrix
+
+        :return: The vocabulary size of the embedding matrix
+        :rtype: int
+        """
         return self.v
     @property
     def vocab(self) -> set[str]:
+        """Vocabulary of the embedding matrix
+        
+        Set of words in the embedding matrix
+
+        :return: The vocabulary of the embedding matrix
+        :rtype: set[str]
+        """
         return set(self.word2idx.keys())
     def __getitem__(self, word:str) -> np.ndarray:
         return self.embedding_matrix[self.word2idx[word]]
